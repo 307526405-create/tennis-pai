115ed4c7d37687d35ef779b8f8cbac4bb7cb6501
307526405-create <307526405@qq.com>
2026-06-18 19:05:23 +0800

    搜索页+名片搜索+装备购买链接(复制跳转)

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,5 +5,9 @@ Page({
     const id = parseInt(options.id) || 0;
     this.setData({ statusBar: wx.getSystemInfoSync().statusBarHeight, r: app.globalData.rackets[id] || app.globalData.rackets[0] });
   },
-  back() { wx.navigateBack(); }
+  back() { wx.navigateBack(); },
+  goBuy(e) {
+    const link = e.currentTarget.dataset.link;
+    if (link) wx.setClipboardData({ data: link, success: () => wx.showToast({ title: '链接已复制，请打开淘宝/京东' }) });
+  }
 });
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,1 +1,1 @@
-Page({data:{s:44},onLoad(){this.setData({s:wx.getSystemInfoSync().statusBarHeight})},eq(){wx.navigateTo({url:'/pages/detail/detail?id=0'})},onShow(){if(typeof this.getTabBar==='function'&&this.getTabBar())this.getTabBar().setData({selected:2})}});
+Page({data:{s:44},onLoad(){this.setData({s:wx.getSystemInfoSync().statusBarHeight})},eq(){wx.navigateTo({url:'/pages/detail/detail?id=0'})},toSearch(){wx.navigateTo({url:'/pages/search/search'})},onShow(){if(typeof this.getTabBar==='function'&&this.getTabBar())this.getTabBar().setData({selected:2})}});
