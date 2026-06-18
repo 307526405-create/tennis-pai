626a0b680c3916ae3912ac4b21eb65be8d9cbd38
307526405-create <307526405@qq.com>
2026-06-18 18:17:46 +0800

    fix: 去掉cover-view用普通view

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,9 +1,1 @@
-Component({
-  data: { selected: 0 },
-  methods: {
-    switchTab(e) {
-      const data = e.currentTarget.dataset;
-      wx.switchTab({ url: data.path });
-    }
-  }
-});
+Component({data:{selected:0},methods:{sw(e){const d=e.currentTarget.dataset;wx.switchTab({url:d.path})}}});
