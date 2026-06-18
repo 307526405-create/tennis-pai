b56a9b8e791db2a3d220f06b58cd90d7048df894
307526405-create <307526405@qq.com>
2026-06-18 19:39:08 +0800

    微信授权头像+按距离排序

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -5,5 +5,5 @@ onLoad(){const rs=app.globalData.rackets;this.setData({s:wx.getWindowInfo().stat
 setName(e){this.setData({name:e.detail.value})},setLv(e){this.setData({lv:e.currentTarget.dataset.i})},
 setSt(e){this.setData({st:e.currentTarget.dataset.i})},setRacket(e){const i=parseInt(e.detail.value);this.setData({racket:i,racketName:this.data.rackets[i]})},
 setCourt(e){this.setData({court:e.detail.value})},setCity(e){this.setData({city:e.detail.value})},
-save(){const d=this.data;const p={name:d.name,lv:d.levels[d.lv],st:d.styles[d.st],racket:d.racket,g:app.globalData.rackets[d.racket]?.model,court:d.court,city:d.city};wx.setStorageSync('profile',p);wx.showToast({title:'保存成功'});setTimeout(()=>wx.navigateBack(),500)},
+save(){const d=this.data;const p={name:d.name,lv:d.levels[d.lv],st:d.styles[d.st],racket:d.racket,g:app.globalData.rackets[d.racket]?.model,court:d.court,city:d.city,avatar:d.avatar};wx.setStorageSync('profile',p);wx.showToast({title:'保存成功'});setTimeout(()=>wx.navigateBack(),500)},onAvatar(e){this.setData({avatar:e.detail.avatarUrl})},
 back(){wx.navigateBack()}});
