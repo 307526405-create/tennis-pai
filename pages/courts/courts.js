057216c3bdb16fadca1b781e4f1469c176c67f1e
307526405-create <307526405@qq.com>
2026-06-22 16:56:08 +0800

    上报球场功能(表单+提交审核)

--- pages/courts/courts.js
+++ pages/courts/courts.js
@@ -1,10 +1,14 @@
 var api = require('../../utils/api');
 
 Page({
-  data: { s: 44, courts: [], markers: [] },
+  data: { s: 44, courts: [], markers: [], showForm: false, rname: '', rcity: '', rfields: '' },
   onLoad() {
     var that = this;
     this.setData({ s: wx.getWindowInfo().statusBarHeight });
+    this.loadCourts();
+  },
+  loadCourts() {
+    var that = this;
     try {
       api.get('/api/courts').then(function(res) {
         var data = res.data || res;
@@ -19,8 +23,24 @@ Page({
       }).catch(function() {});
     } catch (e) {}
   },
+  showReport() { this.setData({ showForm: true }); },
+  hideForm() { this.setData({ showForm: false }); },
+  setRname(e) { this.setData({ rname: e.detail.value }); },
+  setRcity(e) { this.setData({ rcity: e.detail.value }); },
+  setRfields(e) { this.setData({ rfields: e.detail.value }); },
+  submitCourt() {
+    var that = this;
+    var d = this.data;
+    if (!d.rname || !d.rcity) { wx.showToast({ title: '请填写名称和城市', icon: 'none' }); return; }
+    try {
+      api.post('/api/courts', { name: d.rname, city: d.rcity, fields: parseInt(d.rfields) || 1, lat: 0, lng: 0, address: '' }).then(function() {
+        wx.showToast({ title: '已提交，审核后显示' });
+        that.setData({ showForm: false, rname: '', rcity: '', rfields: '' });
+      }).catch(function() { wx.showToast({ title: '提交失败', icon: 'none' }); });
+    } catch (e) {}
+  },
   toSearch() { wx.navigateTo({ url: '/pages/search/search' }); },
   onShow() {
-    if (typeof this.getTabBar === 'function' && this.getTabBar()) this.getTabBar().setData({ selected: 1 });
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) this.getTabBar().setData({ selected: 2 });
   }
 });
