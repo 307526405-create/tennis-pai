d8a93174e8333f94bfbcaf0788c96fa3b08a99f6
307526405-create <307526405@qq.com>
2026-06-23 15:15:16 +0800

    球场:回当前位置+距离计算+导航/电话

--- pages/courts/courts.js
+++ pages/courts/courts.js
@@ -1,10 +1,24 @@
 var api = require('../../utils/api');
 
+function getDist(lat1, lng1, lat2, lng2) {
+  var R = 6371;
+  var dLat = (lat2 - lat1) * Math.PI / 180;
+  var dLng = (lng2 - lng1) * Math.PI / 180;
+  var a = Math.sin(dLat/2)*Math.sin(dLat/2) + Math.cos(lat1*Math.PI/180)*Math.cos(lat2*Math.PI/180)*Math.sin(dLng/2)*Math.sin(dLng/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var km = R * c;
+  return km < 1 ? (km*1000).toFixed(0)+'m' : km.toFixed(1)+'km';
+}
+
 Page({
-  data: { s: 44, courts: [], allCourts: [], markers: [], centerLat: 39.92, centerLng: 116.40, activeId: null, curCity: '全部', cityList: [], showForm: false, rn: '', rc: '', rf: '' },
+  data: { s: 44, courts: [], allCourts: [], markers: [], centerLat: 39.92, centerLng: 116.40, 
+    activeId: null, curCity: '全部', cityList: [], showForm: false, rn: '', rc: '', rf: '',
+    myLat: null, myLng: null
+  },
   onLoad() {
     var that = this;
     this.setData({ s: wx.getWindowInfo().statusBarHeight });
+    this.getLocation();
     try {
       api.get('/api/courts').then(function(res) {
         var data = res.data || res;
@@ -12,6 +26,13 @@ Page({
           var cities = [];
           var seen = {};
           data.forEach(function(c) { if (!seen[c.city]) { seen[c.city]=1; cities.push(c.city); } });
+          // 算距离
+          if (that.data.myLat) {
+            data = data.map(function(c) {
+              c.dist = getDist(that.data.myLat, that.data.myLng, c.lat, c.lng);
+              return c;
+            });
+          }
           that.setData({ allCourts: data, courts: data, cityList: cities });
           that.buildMarkers(data);
           var bj = data.filter(function(c) { return c.city === '北京'; });
@@ -19,6 +40,19 @@ Page({
         }
       }).catch(function() {});
     } catch (e) {}
+  },
+
+  getLocation() {
+    var that = this;
+    wx.getLocation({ type: 'gcj02', success: function(res) {
+      that.setData({ myLat: res.latitude, myLng: res.longitude });
+    }, fail: function() {} });
+  },
+
+  goMyLoc() {
+    if (this.data.myLat) {
+      this.setData({ centerLat: this.data.myLat, centerLng: this.data.myLng });
+    }
   },
 
   setCity(e) {
@@ -32,7 +66,6 @@ Page({
   },
 
   buildMarkers(data) {
-    var that = this;
     var markers = data.map(function(c) {
       return { id: c.id, latitude: c.lat, longitude: c.lng, title: c.name, width: 30, height: 30, callout: { content: c.name, fontSize: 12, padding: 6, bgColor: '#1B5E3B', color: '#fff', display: 'BYCLICK', borderRadius: 4 } };
     });
@@ -41,15 +74,7 @@ Page({
 
   onTap(e) {
     var item = e.currentTarget.dataset.item;
-    this.setData({
-      centerLat: item.lat,
-      centerLng: item.lng,
-      activeId: item.id,
-      markers: this.data.markers.map(function(m) {
-        m.callout = m.id === item.id ? { content: m.title, fontSize: 12, padding: 6, bgColor: '#1B5E3B', color: '#fff', display: 'ALWAYS', borderRadius: 4 } : (m.callout ? Object.assign({}, m.callout, {display: 'BYCLICK'}) : m.callout);
-        return m;
-      })
-    });
+    this.setData({ centerLat: item.lat, centerLng: item.lng, activeId: item.id });
   },
 
   toSearch() { wx.navigateTo({ url: '/pages/search/search' }); },
