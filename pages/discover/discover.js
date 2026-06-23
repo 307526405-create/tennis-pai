afcb657edd94f18ef1a3dd5a7f85231bc3a81785
307526405-create <307526405@qq.com>
2026-06-23 16:14:31 +0800

    发现页球员卡片加快捷关注按钮★/☆

--- pages/discover/discover.js
+++ pages/discover/discover.js
@@ -1,8 +1,10 @@
 var api = require('../../utils/api');
 
-function mapPlayer(p) {
+var currentUserId = 1;
+
+function mapPlayer(p, favIds) {
   var n = p.name || '';
-  return { id: p.id, n: n, a: n[0] || '?', c: '#2E8B57', lv: p.level || '', g: (p.equipment && p.equipment.model) || '', el: String(p.elo || '1500'), pct: p.win_rate != null ? Math.round(p.win_rate * 100) + '%' : '0%', gm: p.match_count || 0, active: !!p.active };
+  return { id: p.id, n: n, a: n[0] || '?', c: '#2E8B57', lv: p.level || '', g: (p.equipment && p.equipment.model) || '', el: String(p.elo || '1500'), pct: p.win_rate != null ? Math.round(p.win_rate * 100) + '%' : '0%', gm: p.match_count || 0, active: !!p.active, fav: favIds.indexOf(p.id) >= 0 };
 }
 
 Page({
@@ -10,15 +12,42 @@ Page({
   onLoad() {
     var that = this;
     this.setData({ s: wx.getWindowInfo().statusBarHeight });
+    this.loadData();
+  },
+
+  loadData() {
+    var that = this;
     try {
-      api.get('/api/players').then(function(res) {
-        var data = res.data || res;
-        if (data && data.length > 0) that.setData({ ps: data.map(mapPlayer) });
-      }).catch(function() {});
+      api.get('/api/players/' + currentUserId + '/favorites').then(function(fres) {
+        var fdata = fres.data || fres;
+        var favIds = (fdata && fdata.favorites || fdata || []).map(function(f) { return f.id; });
+        api.get('/api/players').then(function(res) {
+          var data = res.data || res;
+          if (data && data.length > 0) that.setData({ ps: data.map(function(p) { return mapPlayer(p, favIds); }) });
+        }).catch(function() {});
+      }).catch(function() {
+        api.get('/api/players').then(function(res) {
+          var data = res.data || res;
+          if (data && data.length > 0) that.setData({ ps: data.map(function(p) { return mapPlayer(p, []); }) });
+        }).catch(function() {});
+      });
     } catch (e) {}
   },
   toP(e) {
     wx.navigateTo({ url: '/pages/profile/profile?id=' + e.currentTarget.dataset.id });
+  },
+  toggleFav(e) {
+    var that = this;
+    var id = e.currentTarget.dataset.id;
+    var idx = e.currentTarget.dataset.index;
+    try {
+      api.post('/api/players/' + id + '/favorite', { user_id: currentUserId }).then(function(res) {
+        var d = res.data || res;
+        var ps = that.data.ps;
+        ps[idx].fav = d.favorited;
+        that.setData({ ps: ps });
+      }).catch(function() {});
+    } catch (e) {}
   },
   toSearch() { wx.navigateTo({ url: '/pages/search/search' }); },
   onShow() {
@@ -27,14 +56,5 @@ Page({
     if (typeof this.getTabBar === 'function' && this.getTabBar()) this.getTabBar().setData({ selected: 0 });
   },
   pickCity() { wx.navigateTo({ url: '/pages/city/city' }); },
-  onPullDownRefresh() {
-    var that = this;
-    try {
-      api.get('/api/players').then(function(res) {
-        var data = res.data || res;
-        if (data && data.length > 0) that.setData({ ps: data.map(mapPlayer) });
-      }).catch(function() {});
-    } catch (e) {}
-    setTimeout(function() { wx.stopPullDownRefresh(); }, 500);
-  }
+  onPullDownRefresh() { this.loadData(); setTimeout(function() { wx.stopPullDownRefresh(); }, 500); }
 });
